48ef122adb4366715fc2bbe8b14a0ed46cc540c9
Alona Harnyk <alyonagarnyk@gmail.com>
2022-10-13 22:12:06 +0300

    13.10

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+const form = document.querySelector("form");
 const list = document.querySelector(".list");
+const button = document.querySelector(".more");
 
 const BASE_URL = "https://app.ticketmaster.com/discovery/v2/events.json";
 const API = "9cTjAjlRB53wyhAFk5VzXcBu5GiPU6fK";
 
-function fetchEvent() {
+let pageToFetch = 0;
+let keyword = "";
+
+function fetchEvent(page, keyword) {
   const params = new URLSearchParams({
     apikey: API,
+    page,
+    keyword,
+    size: 50,
   });
 
   return fetch(`${BASE_URL}?${params}`)
@@ -18,14 +26,31 @@ function fetchEvent() {
     .catch((error) => console.log(error));
 }
 
-function getEvents() {
-  fetchEvent().then((data) => {
-    const events = data._embedded.events;
-    renderEvents(events);
+function getEvents(page, keyword) {
+  fetchEvent(page, keyword).then((data) => {
+    console.log(data.page.totalElements);
+
+    if (data.page.totalElements === 0) {
+      button.classList.add("invisible");
+      alert(`There are no events by keyword ${keyword}`);
+    }
+
+    const events = data?._embedded?.events;
+    if (events) {
+      renderEvents(events);
+    }
+
+    if (pageToFetch === data.page.totalPages - 1) {
+      button.classList.add("invisible");
+      alert("Finish");
+      return;
+    }
+    pageToFetch += 1;
+    if (data.page.totalPages > 1) {
+      button.classList.remove("invisible");
+    }
   });
 }
-
-getEvents();
 
 function renderEvents(events) {
   const markup = events
@@ -36,5 +61,21 @@ function renderEvents(events) {
     </li>`;
     })
     .join("");
-  list.innerHTML = markup;
+  list.insertAdjacentHTML("beforeend", markup);
 }
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const query = event.target.elements.query.value;
+  keyword = query;
+  pageToFetch = 0;
+  list.innerHTML = "";
+  if (!query) {
+    return;
+  }
+  getEvents(pageToFetch, query);
+});
+
+button.addEventListener("click", () => {
+  getEvents(pageToFetch, keyword);
+});
